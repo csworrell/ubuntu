dc875449d5dc9d78dc1aeb5236d415f456a0f91b
csworrell <146026292+csworrell@users.noreply.github.com>
2024-02-23 11:50:28 -0600

    Update bcrk.sh

--- bcrk.sh
+++ bcrk.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export DISPLAY=:0
-nohup google-chrome-stable --kiosk --password-store=basic --noerrdialogs --incognito --kiosk-printing --simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT' https://ffprod.millsfleetfarm.com/ff-kiosk/ > /dev/null 2>&1 &
+Exec=/usr/bin/google-chrome-stable --kiosk --password-store=basic --noerrdialogs --incognito --use-system-default-printer --kiosk-printing --disable-pinch --simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT' https://ffprod.millsfleetfarm.com/ff-kiosk/
